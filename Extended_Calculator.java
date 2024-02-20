ddcd718b1dbeb66663fd3733dec601948a4cc0f5
01fe22bca128 <157695122+01fe22bca128@users.noreply.github.com>
2024-02-20 10:46:57 +0530

    code for extended calculator

---
+++ Extended_Calculator.java
@@ -0,0 +1,16 @@
+/**
+ * The Arithmatic_Op class contains methods for performing arithmetic operations such as squaring,
+ * cubing, and finding the modulus of two numbers.
+ */
+public class Extended_Calculator{
+    int square(int operand_1){
+        return(operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+    float modulus(float operand_1,float operand_2){
+        return(operand_1%operand_2);
+    }
+   
+}
